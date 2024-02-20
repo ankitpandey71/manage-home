90b517f2ca83ab8740c59f4c9a1ad51551bbd81b
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2024-02-20 14:50:30 +0530

    [ADD] component

---
+++ src/navigation/index.js
@@ -0,0 +1,4 @@
+import CalendarNavigation from './calender-navigation';
+import Routes from './routes';
+
+export { CalendarNavigation, Routes };
---
+++ src/navigation/routes.js
@@ -0,0 +1,7 @@
+const Routes = {
+    HOME: 'home',
+    CREATE_TASK: 'CreateTask'
+  };
+  
+  export default Routes;
+  
